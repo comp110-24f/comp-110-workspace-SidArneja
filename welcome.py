7b98f354a45ca8ccd4ca711c57a3aecc3c9014bd
sidarneja <arsi@unc.edu>
2024-08-27 16:48:14 -0400

    Finished Exercise 00

--- welcome.py
+++ welcome.py
@@ -1,7 +1,4 @@
-"""A welcoming test program to start COMP110"""
-
-__author__ = "01234567"
-
-print("Welcome to COMP110!")
-print("You are in for a fun adventure into programming!")
-print("<3 the COMP110 Team!")
+def sum(num1: int, num2: int) -> int:
+   print(num1 + num2)
+    
+sum(num1 = 38, num2 = 76)
