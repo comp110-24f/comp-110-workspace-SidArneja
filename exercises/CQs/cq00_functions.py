13875c052e56bcaa0a997602a5119a1d8523d451
sidarneja <arsi@unc.edu>
2024-08-28 10:54:52 -0400

    Challenge Question 1

---
+++ exercises/CQs/cq00_functions.py
@@ -0,0 +1,17 @@
+"""My first Challenge question in COMP110"""
+
+__author__ = "730751163"
+
+
+def mimic(message: str) -> str:
+    """Defining the function mimic as a string type"""
+    return message
+
+
+def main() -> None:
+    """Defining the main function"""
+    print(mimic(message=input("What is your message?")))
+
+
+if __name__ == "__main__":
+    main()
